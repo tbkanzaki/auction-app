e9618169d13c97e2d9af7e7721f9d0ee2f6dd43d
TEREZA KANZAKI <tkanzaki@gmail.com>
2023-05-02 11:21:45 -0300

    Usuário admin se autentica com sucesso e faz logout

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -18,8 +18,29 @@ describe 'Usuário admin se autentica' do
     expect(page).to have_content 'Login efetuado com sucesso.'
     within('nav') do
       expect(page).not_to have_link 'Entrar'
-      expect(page).to have_link 'Sair'
+      expect(page).to have_button 'Sair'
       expect(page).to have_content 'Tereza Barros - tereza@leilaodogalpao.com.br'
     end
+  end
+
+  it 'e faz logout' do
+    # Arrange
+    User.create!(name: 'Tereza Barros', email:'tereza@leilaodogalpao.com.br', password:'senha1234', cpf: '38001357287')
+
+    # Act
+    visit('/')
+    click_on 'Entrar'
+    within('form') do
+      fill_in 'E-mail', with: 'tereza@leilaodogalpao.com.br'
+      fill_in 'Senha', with: 'senha1234'
+      click_on 'Entrar'
+    end
+    click_on 'Sair'
+
+    # Assert
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    expect(page).to have_link 'Entrar'
+    expect(page).not_to have_button 'Sair'
+    expect(page).not_to have_content 'Tereza Barros - tereza@leilaodogalpao.com.br'
   end
 end
