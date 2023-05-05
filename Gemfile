dc839b0e8bd93c0caa51d6033c7e22af8450d3d2
TEREZA KANZAKI <tkanzaki@gmail.com>
2023-05-05 12:58:37 -0300

    Adicionando Bootstrap e MyStyle

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem "sqlite3", "~> 1.4"
 gem "puma", "~> 5.0"
 
 gem "devise"
+gem 'bootstrap'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -8,7 +8,6 @@ describe 'Usuário visita página inicial' do
     visit('/')
 
     # Assert
-    expect(page).to have_content('Leilão de Estoque')
     expect(page).to have_link 'Entrar'
     expect(page).not_to have_content 'Cadastrar Categoria'
   end
